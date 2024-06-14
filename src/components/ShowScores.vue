dcd8316f00ce942790f5898c558fcf5b83e99ee9
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2024-06-14 13:45:21 +0200

    :lipstick: :sparkles: add showscores functionality + style

---
+++ src/components/ShowScores.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { IGameState } from '../models/IGameState';
+import { defineProps } from 'vue';
+
+const props = defineProps<{
+  gameState: IGameState;
+}>();
+</script>
+
+<template>
+    <aside class="scoreShow">
+      <h3>Resultat</h3>
+      <p>{{ props.gameState.users.nameO }}: {{ props.gameState.scores.scoresO }} poäng.</p>
+      <p> {{ props.gameState.users.nameX }}: {{ props.gameState.scores.scoresX }} poäng.</p>
+    </aside>
+</template>
+
+<style scoped>
+h3 {
+  font-family: 'Times New Roman', Times, serif;
+  text-transform: uppercase;
+  margin-bottom: 10px;
+  font-size: 3rem;
+}
+</style>
