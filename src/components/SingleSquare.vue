6fd33e924bf9feaddabdce1ff3ee3d218bb9a029
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2024-06-14 13:46:44 +0200

    ✨ :lipstick: add singlesquare functionality + styling

---
+++ src/components/SingleSquare.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps<{
+  state: string;
+}>();
+
+const emit = defineEmits<{
+  (e: 'squareClicked'): void;
+}>();
+
+const playGame = () => {
+  if (props.state === '') {
+    emit('squareClicked');
+  }
+};
+</script>
+
+<template>
+    <div class="gameSquare" @click="playGame">
+      <p class="gameValue">{{ state }}</p>
+    </div>
+</template>
+
+<style scoped>
+.gameSquare {
+  border: 2px rgb(114, 5, 27) solid;
+  overflow: hidden;
+}
+
+.gameSquare:hover {
+  background-color: rgb(227, 176, 189);
+}
+
+.gameValue {
+  font-size: 2rem;
+}
+</style>
+
+export default {};
