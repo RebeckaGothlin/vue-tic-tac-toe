0f2ba1787c41cf5b0d925cbf994e5537fbae0063
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2024-06-17 09:09:42 +0200

    🔥 remove comment

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -36,12 +36,12 @@ function checkIfGameOver(index: number) {
   const winningCombinations = [
     [0, 1, 2],
     [3, 4, 5],
-    [6, 7, 8], // Rader
+    [6, 7, 8],
     [0, 3, 6],
     [1, 4, 7],
-    [2, 5, 8], // Kolumner
+    [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6], // Diagonaler
+    [2, 4, 6] 
   ];
 
   const currentPlayer = props.gameState.gameboard[index];
