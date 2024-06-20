ba0432febbf3dee0691c693e042d4f4d55109afb
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2024-06-20 10:15:19 +0200

    💄 add styling

--- src/components/AskUsername.vue
+++ src/components/AskUsername.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { defineProps, defineEmits, ref } from 'vue';
-import { IGameState } from '../models/IGameState';
+import { defineProps, defineEmits, ref } from "vue";
+import { IGameState } from "../models/IGameState";
 
 let name = ref("");
 
@@ -10,22 +10,22 @@ const props = defineProps<{
 }>();
 
 const emit = defineEmits<{
-  (e: 'usernameSubmitted', payload: { xoro: string; name: string }): void;
+  (e: "usernameSubmitted", payload: { xoro: string; name: string }): void;
 }>();
 
 const submitUserName = () => {
-  if (props.xoro === 'X') {
+  if (props.xoro === "X") {
     props.gameState.users.nameX = name.value;
   } else {
     props.gameState.users.nameO = name.value;
   }
-  emit('usernameSubmitted', { xoro: props.xoro, name: name.value });
+  emit("usernameSubmitted", { xoro: props.xoro, name: name.value });
 };
 </script>
 
 <template>
-  <div class="welcome">
-    <h1>Tic Tac Toe</h1>
+  <div class="firstPage">
+    <h1>Tre i rad</h1>
     <p class="instruction">Få tre av dina symboler (X eller O) i rad horisontellt, vertikalt eller diagonalt. Den första spelaren som lyckas med detta vinner. Om alla rutor fylls utan att någon får tre i rad slutar spelet oavgjort.</p>
     <p>Fyll i namnen på spelare X och O för att börja spelet.</p>
     <label>Namn på spelare {{ xoro }}</label>
@@ -36,15 +36,24 @@ const submitUserName = () => {
 
 <style scoped>
 h1 {
-  margin-top: 10px;
+  font-size: 4rem;
+  margin-top: 5px;
+  font-family: 'Times New Roman', Times, serif;
 }
 
 .usernameBtn {
   border-radius: 16px;
-  padding: 12px;
+  padding: 10px;
   background-color: rgb(114, 5, 27);
   color: rgb(255, 200, 213);
   margin: 10px;
+  text-transform: uppercase;
+  transition: 0.3s ease;
+}
+
+.usernameBtn:hover {
+  background-color: rgb(70, 2, 16);
+  border: 1px solid rgb(255, 200, 213);
 }
 
 input {
@@ -54,37 +63,46 @@ input {
   color: rgb(114, 5, 27);
   font-weight: bolder;
   background-color: rgb(255, 200, 213);
+  transition: 0.3s ease
+}
+
+input:hover {
+  background-color: rgb(227, 176, 189);
 }
 
 label {
-  font-size: 1.5rem;
+  font-size: 1.2rem;
   margin: 16px;
 }
 
 .instruction {
   margin: 0 auto;
-  font-weight: 500;
+  font-weight: 700;
   margin-bottom: 50px;
   max-width: 400px;
   text-align: center;
+  font-size: 1.2rem;
 }
 
 p {
   line-break: auto;
 }
 
-.welcome {
+.firstPage {
   color: rgb(114, 5, 27);
 }
 
 @media (width <= 740px) {
-  .welcome {
+  .firstPage{
       max-width: 480px;
   }
 
   h1 {
-    font-size: 48px;
-    font-family: 'Times New Roman', Times, serif;
+    font-size: 3.5rem;
+  }
+
+  .instruction {
+    font-size: 1rem;
   }
 }
 </style>
